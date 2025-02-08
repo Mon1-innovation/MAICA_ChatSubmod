4b6e5c23f2de655ab687008ef9866b97d8bb5f15
edgeinfinity1 <jiahaohe39@gmail.com>
2025-02-08 22:49:16 +0800

    fix

--- game/python-packages/emotion_analyze_v2.py
+++ game/python-packages/emotion_analyze_v2.py
@@ -85,7 +85,7 @@ class EmoSelector:
         # 正则表达式模式
         pattern = r'\[(.*?)\]'
         # Filter emojis
-        bad_pattern = r'[\U0001f0a0-\U0001f0ff\U0001f170-\U0001f19a\U0001f300-\U0001faff\u2190-\u21FF\u2300-\u23FF\u2500-\u26FF\u2700-\u27BF\u27F0-\u27FF\u2900-\u297F\u2B00-\u2BFF]+'
+        bad_pattern = r'(\ud83c[\udf00-\udfff])|(\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55]'
         message = re.sub(bad_pattern, '', message)
         # 查找所有匹配的内容
         matches = re.findall(pattern, message)
