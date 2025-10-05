c7c4577ea726e4a505848ffd73cc6ee88604a4ec
PencilMario <Sir.P@foxmail.com>
2025-10-05 22:15:23 +0800

    成功不log了

--- game/python-packages/maica.py
+++ game/python-packages/maica.py
@@ -1116,7 +1116,7 @@ t9vozy56WuHPfv3KZTwrvZaIVSAExEL17wIDAQAB
                 data = res.json()
                 if data["success"]:
                     self.workload_raw = data["content"]
-                    logger.debug("Workload updated successfully.")
+                    #logger.debug("Workload updated successfully.")
                 else:
                     logger.error("Failed to update workload: {}".format(data))
             else:
